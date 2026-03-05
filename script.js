bab414ee0d990656a635b33757661e7fb49ae2db
dev-parkavan7 <developers.parkavanseek4@gmail.com>
2026-03-05 16:50:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ behavior:"smooth"
 
 })
 })
+
+const sections = document.querySelectorAll(".section");
+
+window.addEventListener("scroll", () => {
+  sections.forEach(section => {
+    const sectionTop = section.getBoundingClientRect().top;
+
+    if(sectionTop < window.innerHeight - 100){
+      section.classList.add("show");
+    }
+  });
+});
